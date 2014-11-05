91a8843c0ebe7f81d243b913ceec57a0622f15cd
karrde00 <karrde00@gmail.com>
2014-11-05 13:09:31 -0600

    test stuff

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -15,9 +15,11 @@ class HomeController extends BaseController {
 	|
 	*/
 
-	public function showWelcome()
+	protected $layout = 'layouts.master';
+
+	public function index()
 	{
-		return View::make('hello');
+		return View::make('home');
 	}
 
 }
