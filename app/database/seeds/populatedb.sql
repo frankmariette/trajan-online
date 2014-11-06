447efe04ebd15ed956673eeaa00a002e39d62e62
Jonathan Kendrick <jckv22@mail.missouri.edu>
2014-11-05 20:15:34 -0600

    Finish Population

--- app/database/seeds/populatedb.sql
+++ app/database/seeds/populatedb.sql
@@ -42,7 +42,6 @@ CREATE TABLE playerboard (
 DROP TABLE IF EXISTS trajan.TileType;
 CREATE TABLE TileType (
 	typeid Int PRIMARY KEY,
-	TileTypeid Int,
 	types varchar(50)
 );
 DROP TABLE IF EXISTS trajan.TrajanTile;
@@ -109,17 +108,69 @@ CREATE TABLE GameBoardTile (
 	tileid Int REFERENCES TileType(typeid)
 );
 
-INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp)
-VALUES ('Scroll', 'Water', 3, 3, 'Scroll', 'Water', 2, 2);
-INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp)
-VALUES ('Wine', 'Fish', 3, 3, 'Wine', 'Fish', 2, 2);
-INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp)
-VALUES ('Gold', 'Diamonds', 3, 3, 'Gold', 'Diamonds', 2, 2);
-INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp)
-VALUES ('Columns', 'BearPelt', 3, 3, 'Columns', 'BearPelt', 2, 2);
-INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp)
-VALUES ('Fruit', 'Pottery', 3, 3, 'Fruit', 'Pottery', 2, 2);
-INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp)
-VALUES ('Cow', 'Wheat', 3, 3, 'Cow', 'Wheat', 2, 2);
-INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp)
-VALUES ('Soldier', NULL, 2, NULL, 
+
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Scroll', 'Water', 3, 3, 'Scroll', 'Water', 2, 2, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Wine', 'Fish', 3, 3, 'Wine', 'Fish', 2, 2, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Gold', 'Diamonds', 3, 3, 'Gold', 'Diamonds', 2, 2, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Columns', 'BearPelt', 3, 3, 'Columns', 'BearPelt', 2, 2, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Fruit', 'Pottery', 3, 3, 'Fruit', 'Pottery', 2, 2, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Cow', 'Wheat', 3, 3, 'Cow', 'Wheat', 2, 2, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Soldier', NULL, 2, NULL, 'Soldier', NULL, 1, NULL, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Builder', NULL, 2, NULL, 'Builder', NULL, 1, NULL, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Bonus Tile', NULL, 3, NULL, 'Bonus Tile', NULL, 2, NULL, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VAlUES ('Bread', NULL, 9, NULL, 'Bread', NULL, 6, NULL, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Games', NULL, 9, NULL, 'Games', NULL, 6, NULL, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Religion', NULL, 9, NULL, 'Religion', NULL, 6, NULL, 5);
+
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (2, 'Fountain', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (3, 'Fountain', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (4, 'Fountain', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (5, 'Fountain', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (2, 'Stairs', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (3, 'Stairs', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (4, 'Stairs', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (5, 'Stairs', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (2, 'Window', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (3, 'Window', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (4, 'Window', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (5, 'Window', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (2, 'Door', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (3, 'Door', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (4, 'Door', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (5, 'Door', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (2, 'Roof', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (3, 'Roof', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (4, 'Roof', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (5, 'Roof', 3);
