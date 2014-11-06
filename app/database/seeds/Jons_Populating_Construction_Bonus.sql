6064ca0526fe2ce75b6bae9019de386fcae3727b
Jonathan Kendrick <jckv22@mail.missouri.edu>
2014-11-06 16:39:31 -0600

    Pushing my construction and bonus file

---
+++ app/database/seeds/Jons_Populating_Construction_Bonus.sql
@@ -0,0 +1,65 @@
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Scroll', 'Water', 3, 3, 'Scroll', 'Water', 2, 2, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Wine', 'Fish', 3, 3, 'Wine', 'Fish', 2, 2, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Gold', 'Diamonds', 3, 3, 'Gold', 'Diamonds', 2, 2, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Columns', 'BearPelt', 3, 3, 'Columns', 'BearPelt', 2, 2, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Fruit', 'Pottery', 3, 3, 'Fruit', 'Pottery', 2, 2, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Cow', 'Wheat', 3, 3, 'Cow', 'Wheat', 2, 2, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Soldier', NULL, 2, NULL, 'Soldier', NULL, 1, NULL, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Builder', NULL, 2, NULL, 'Builder', NULL, 1, NULL, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Bonus Tile', NULL, 3, NULL, 'Bonus Tile', NULL, 2, NULL, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VAlUES ('Bread', NULL, 9, NULL, 'Bread', NULL, 6, NULL, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Games', NULL, 9, NULL, 'Games', NULL, 6, NULL, 5);
+INSERT INTO BonusCards (gold_one, gold_two, gold_one_vp, gold_two_vp, gray_one, gray_two, gray_one_vp, gray_two_vp, TileTypeid)
+VALUES ('Religion', NULL, 9, NULL, 'Religion', NULL, 6, NULL, 5);
+
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (2, 'Fountain', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (3, 'Fountain', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (4, 'Fountain', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (5, 'Fountain', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (2, 'Stairs', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (3, 'Stairs', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (4, 'Stairs', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (5, 'Stairs', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (2, 'Window', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (3, 'Window', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (4, 'Window', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (5, 'Window', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (2, 'Door', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (3, 'Door', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (4, 'Door', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (5, 'Door', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (2, 'Roof', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (3, 'Roof', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (4, 'Roof', 3);
+INSERT INTO ConstructionTile (vp, types, TileTypeid)
+VALUES (5, 'Roof', 3);
