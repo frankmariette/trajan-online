880c21e3243b75b8b995ca30cb64c63aeb26fbee
ccooksey13 <crcxwc@mail.missouri.edu>
2014-11-09 19:59:38 -0600

    move .json files to public folder

--- app/views/gamePage.blade.php
+++ app/views/gamePage.blade.php
@@ -6,18 +6,20 @@
 @section('body');
   <script src='//cdnjs.cloudflare.com/ajax/libs/three.js/r69/three.js'></script>
   <script src='//cdnjs.cloudflare.com/ajax/libs/three.js/r69/three.min.js'></script>
+  <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
   <script>
+
       init();
       animate();
-      var mesh;
+      var mesh, renderer, scene, camera;
 
     function init(){
-      var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+      scene = new THREE.Scene();
+			camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-			var renderer = new THREE.WebGLRenderer();
+			renderer = new THREE.WebGLRenderer();
 			renderer.setSize( window.innerWidth, window.innerHeight );
-			//document.body.appendChild( renderer.domElement );
 
 			var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -25,13 +27,13 @@
 			scene.add( cube );
 
       var loader = new THREE.JSONLoader(true);
-      loader.load("/blender_scene/gameBoard.json", function(geometry, materials){
+      loader.load("models/gameBoardNoT.json", function(geometry, materials){
         mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
         mesh.scale.set(10, 10, 10);
         mesh.position.y = 150;
         mesh.position.x = 0;
         scene.add(mesh);
-      });
+      }, "models/gameBoardTexture.tif");
 			camera.position.z = 5;
 
       document.body.appendChild( renderer.domElement );
@@ -40,14 +42,12 @@
     function animate(){
 			var render = function () {
 				requestAnimationFrame( render );
-
-				mesh.position.y = 50;
-        mesh.position.x = 0;
-
 				renderer.render(scene, camera);
+        //mesh.rotation.x += 1;
 			};
 
 			render();
     }
+
   </script>
 @stop
