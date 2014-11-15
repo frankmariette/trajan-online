ee128a91a4d4466aade64c0dbc9c0a2a0d037cba
aadq67_113114 <aadq67@babbage.cs.missouri.edu>
2014-11-15 11:08:26 -0600

    added seaport class/functions

--- app/controllers/seaport.php
+++ app/controllers/seaport.php
@@ -4,14 +4,30 @@ class Seaport{
 	public $faceDownCards;
 	public $discardOne;
 	public $discardTwo;
-	public function doAction();
-	public function tradeCommodity(&$playerCards){
+	public function doAction(){}
+	public function shipCommodity(&$playerCards){
+			
+	}
+	public function drawTwo(&$faceDownCards, &$discardOne, &$discardTwo){
+		//add two random cards to player hand
+		$playerCards[] = array_rand($faceDownCards);
+		$playerCards[] = array_rand($faceDownCards);
+		//discard one
+		$choose = rand(1,2);
+		if($choose == 1)
+			$discardOne = $playerCards[$disc_id];
+		else
+			$discardTwo = $playerCards[$disc_id];
+		unset($playerCards[$disc_id]);
+	}
+	public function drawOne(&$discardOne, &$discardTwo){
+		if($choice == 1)
+			$playerCards[] = array_rand($discardOne);
+		else
+			$playerCards[] = array_rand($discardTwo);
+	}
+	public function playCards(&$playerCards){
 	
 	}
-	public function drawTwo(&$faceDownCards){
-		$playerCards[] = array_rand($faceDownCards);
-		$playerCards[] = array_rand($faceDownCards);
-	}
-	public function drawOne($discardOne, $discardTwo);
 }
 ?>
