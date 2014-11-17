a6b9f88572c6ae8048dd7d13cf394bb7cdba07b3
karrde00 <karrde00@gmail.com>
2014-11-16 19:04:52 -0600

    Adds senate actions

---
+++ app/actions/SenateAction.php
@@ -0,0 +1,32 @@
+<?php namespace actions;
+
+class SenateAction {
+
+  protected $currentTileSpotPoints;
+  protected $player;
+
+  public function __construct($playerId, $currentTileSpotPoints){
+    $this->player = new User($playerId);
+    $this->currentTileSpotPoints = $currentTileSpotPoints;
+  }
+
+  public function advanceTile(){
+    if ($this->checkCurrentTileSpace < 8){
+      $nextSpacePoints = $this->getNextTilesVictoryPoints();
+      $this->addVictoryPointsToPlayerScore($nextSpacePoints);
+      return true;
+    }
+
+    return false;
+  }
+
+  private function addVictoryPointsToPlayerScore($points){
+    $this->player->addVictoryPoints($points);
+  }
+
+  private function getNextTilesVictoryPoints(){
+    // This function called doesnt exist. Estimation for how it would work.
+    return GameBoard::returnNextLocationVictoryPoints($this->currentTileSpot);
+  }
+
+}
