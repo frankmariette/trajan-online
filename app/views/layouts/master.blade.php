06c375b8311464d1a2769772edb91df77a95572c
karrde00 <karrde00@gmail.com>
2014-11-18 02:17:48 -0600

    makes the chat look nicer

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{HTML::style('http://maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css')}}
     {{HTML::script('http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js')}}
+    {{HTML::script('http://maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js')}}
     {{HTML::script('js/brain-socket.min.js')}}
+    {{HTML::style('/css/custom.css')}}
     <style>
         body{
             padding-top: 70px;
@@ -63,81 +65,78 @@
     </div>
     @yield('body')
 </div>
-@if (Auth::check())
-<div class="messenger bg-white">
-<div class="chat-header text-white bg-gray-dark">
-    Real-time Chat
-    <a href="#" id="chat-toggle" class="pull-right chat-toggle">
-        <span class="glyphicon glyphicon-chevron-down"></span>
-    </a>
-</div>
-<div class="messenger-body open">
-    <ul class="chat-messages" id="chat-log">
+    @if (Auth::check())
+    <section class="module">
+      <header class="top-bar">
+        <h1>Group Chat</h1>
+      </header>
+      <div class="messenger-body open">
+          <ol class="discussion" id="chat-log">
 
-    </ul>
-    <div class="chat-footer">
-        <div class="p-lr-10">
-            <input type="text" id="chat-message"
-                class="input-light input-large brad chat-search" placeholder="Your message...">
-        </div>
-    </div>
-</div>
-</div>
-<script>
-$(function(){
+          </ol>
+          <div class="chat-footer">
+              <div class="p-lr-10">
+                  <input type="text" id="chat-message"
+                      class="input-light input-large brad chat-search" placeholder="Your message...">
+              </div>
+          </div>
+      </div>
+    </section>
+  <script>
+  $(function(){
 
-    // var user_id = Math.floor((Math.random()*1000)+1);
-    var user_id = {{ Auth::user()->id }};
-    //make sure to update the port number if your ws server is running on a different one.
-    window.app = {};
+      // var user_id = Math.floor((Math.random()*1000)+1);
+      var user_id = {{ Auth::user()->id }};
+      //make sure to update the port number if your ws server is running on a different one.
+      window.app = {};
 
-    app.BrainSocket = new BrainSocket(
-            new WebSocket('ws://localhost:8080'),
-            new BrainSocketPubSub()
-    );
+      app.BrainSocket = new BrainSocket(
+              new WebSocket('ws://localhost:8080'),
+              new BrainSocketPubSub()
+      );
 
-    app.BrainSocket.Event.listen('generic.event',function(msg){
-        console.log(msg);
-        if(msg.client.data.user_id == user_id){
-            $('#chat-log').append('<li><div class="message">'+msg.client.data.user_name+': ' +msg.client.data.message+'</div></li>');
-        }else{
-            var str_test='<li class="right"><div class="message">'+msg.client.data.user_name+': '+msg.client.data.message+'</div></li>';
-            $('#chat-log').append(str_test);
-        }
-    });
+      app.BrainSocket.Event.listen('generic.event',function(msg){
+          console.log(msg);
+          if(msg.client.data.user_id == user_id){
+              $('#chat-log').append(
+                '<li class="self"><div class="avatar"><img src="{{Auth::user()->get_gravatar(Auth::user()->email)}}"/></div><div class="message"><p>'+msg.client.data.user_name+': ' +msg.client.data.message+' {{ date("H:i a", time()) }}</p></div></li>');
+          }else{
+              var str_test='<li class="other"><div class="avatar"><img class="avatar" src="'+msg.client.data.user_pic+'"/></div><div class="message"><p>'+msg.client.data.user_name+': '+msg.client.data.message+' {{ date("H:i a", time()) }}</p></div></li>';
+              $('#chat-log').append(str_test);
+          }
+      });
 
-    app.BrainSocket.Event.listen('app.success',function(data){
-        console.log('An app success message was sent from the ws server!');
-        console.log(data);
-    });
+      app.BrainSocket.Event.listen('app.success',function(data){
+          console.log('An app success message was sent from the ws server!');
+          console.log(data);
+      });
 
-    app.BrainSocket.Event.listen('app.error',function(data){
-        console.log('An app error message was sent from the ws server!');
-        console.log(data);
-    });
+      app.BrainSocket.Event.listen('app.error',function(data){
+          console.log('An app error message was sent from the ws server!');
+          console.log(data);
+      });
 
-    $('#chat-message').keypress(function(event) {
+      $('#chat-message').keypress(function(event) {
 
-        if(event.keyCode == 13){
+          if(event.keyCode == 13){
 
-            app.BrainSocket.message('generic.event',
-                    {
-                        'message':$(this).val(),
-                        'user_id':user_id,
-                        'user_name':'{{ Auth::user()->fname}}'
-                    }
-            );
-            $(this).val('');
+              app.BrainSocket.message('generic.event',
+                      {
+                          'message':$(this).val(),
+                          'user_id':user_id,
+                          'user_name':'{{ Auth::user()->fname}}',
+                          'user_pic': '{{ Auth::user()->get_gravatar(Auth::user()->email) }}'
+                      }
+              );
+              $(this).val('');
 
-        }
+          }
 
-        return event.keyCode != 13; }
-    );
-});
-</script>
-@endif
-</div>
-{{HTML::script('http://maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js')}}
+          return event.keyCode != 13; }
+      );
+  });
+  </script>
+  @endif
 @show
 </body>
 </html>
