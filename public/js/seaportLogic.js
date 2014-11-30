c54860bf1c679576d58a1b0f56be09d649b28f52
atryino <dolon3@gmail.com>
2014-11-30 00:30:33 -0600

    edit turn logic

--- public/js/seaportLogic.js
+++ public/js/seaportLogic.js
@@ -1,3 +1,12 @@
 function seaportLogic(){
 
 }
+//ship sprites go at 165, 600; 450, 430; 305, 520
+function makeActive(currentTile){
+  currentTile.inputEnabled = true;
+  currentTile.events.onInputDown.add(listener, this); //if this piece is clicked, move it
+}
+function listener(tile){ //this is how you add a callback to move a piece!
+  tile.position.x = 0;
+  tile.position.y = 0;
+}
