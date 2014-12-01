d2ca7efe129690c3885f3ac143ee6e144d91c550
karrde00 <karrde00@gmail.com>
2014-11-30 19:23:01 -0600

    basic start. Fixing blue haired girls merge conflicts then coming back to this

--- public/js/mainGame.js
+++ public/js/mainGame.js
@@ -1,3 +1,12 @@
 function Game(){
+
+	// Attribrutes for the game to manage
+	initialize(){
+		this.players = [];
+		this.turn = 0;
+		this.quarter = 0;
+		this.pause = false;
+		this.Phaser = new Phaser.Game(1300, 1800, Phaser.AUTO, 'gameboard', {preload: preload, create: create, update: update});
+	}
 	
 }
