4708b54c52e3a61feaa136779b4f9bbf6ef65b86
ccooksey13 <crcxwc@mail.missouri.edu>
2014-12-05 19:48:18 -0600

    trajan action works, moved assets

--- public/js/mainGame.js
+++ public/js/mainGame.js
@@ -207,10 +207,10 @@ Game.prototype.phaserCreate = function() {
 
   var tTile0 = G.phaser.tTiles.create(253, G.phaser.world.height - 1058, 'tt_VP_yellow_white');
   var tTile1 = G.phaser.tTiles.create(422, G.phaser.world.height - 1058, 'tt_two_cards_pink_green');
-  var tTile2 = G.phaser.tTiles.create(253, G.phaser.world.height - 958, 'tt_two_cards_blue_orange');
-  var tTile3 = G.phaser.tTiles.create(422, G.phaser.world.height - 958, 'tt_move_military_blue_blue');
-  var tTile4 = G.phaser.tTiles.create(253, G.phaser.world.height - 858, 'tt_plus_two_pink_white');
-  var tTile5 = G.phaser.tTiles.create(422, G.phaser.world.height - 858, 'tt_VP_orange_white');
+  var tTile2 = G.phaser.tTiles.create(253, G.phaser.world.height - 968, 'tt_two_cards_blue_orange');
+  var tTile3 = G.phaser.tTiles.create(422, G.phaser.world.height - 968, 'tt_move_military_blue_blue');
+  var tTile4 = G.phaser.tTiles.create(253, G.phaser.world.height - 884, 'tt_plus_two_pink_white');
+  var tTile5 = G.phaser.tTiles.create(422, G.phaser.world.height - 884, 'tt_VP_orange_white');
 
   G.phaser.fTiles = G.phaser.add.group();
 
@@ -502,6 +502,7 @@ Game.prototype.getAction = function(){
 			}
 			else if(currentTray == trajanTray){
 				G.phaser.gameState = 'trajan';
+				G.getTrajanTile();
 			}
 			else if(currentTray == constructionTray){
 				G.phaser.gameState = 'construction';
@@ -870,4 +871,76 @@ Game.prototype.moveWorker = function(littleDude){ //move game piece to correct l
 		littleDude.position.y = G.phaser.leader.getAt(0).position.y + 35;
 		G.phaser.gameState = "bonusAction";
 	}
+}
+
+Game.prototype.getTrajanTile = function(){
+	G.phaser.textAction.text = "Please Select a Trajan Tile";
+	G.phaser.tTiles.forEach(function(tile){
+		tile.inputEnabled = true;
+		tile.events.onInputDown.add(G.trajanLogic, this, G.phaser.trajan.getAt(0));
+	}, this, true);
+}
+
+Game.prototype.trajanLogic = function(currentTile, arch){
+	if(G.phaser.gameState != "trajan"){
+		return;
+	}
+	var trajan_Spaces = [
+		new Phaser.Rectangle(603, G.phaser.world.height-480, 100, 100),
+		new Phaser.Rectangle(830, G.phaser.world.height-470, 100, 100),
+		new Phaser.Rectangle(900, G.phaser.world.height-300, 100, 100),
+		new Phaser.Rectangle(795, G.phaser.world.height-100, 100, 100),
+		new Phaser.Rectangle(630, G.phaser.world.height-139, 100, 100),
+		new Phaser.Rectangle(515, G.phaser.world.height-300, 100, 100),
+		new Phaser.Rectangle(709, G.phaser.world.height-300, 100, 100)
+
+	]
+
+
+	var spaces = [0,0,0,0,0,0];
+	var index = 0;
+	//check if arch is in the middle. If so then don't do anything else.
+	if (trajan_Spaces[6].contains(G.phaser.trajan.getAt(0).position.x, G.phaser.trajan.getAt(0).position.y)){
+		console.log("Can't do the Trajan Action.");
+		return 0;
+	}
+	//check spaces to see if they are occupied.
+	for (i=0;i<6;i++) {
+		if (trajan_Spaces[i].contains(G.phaser.trajan.getAt(0).position.x, G.phaser.trajan.getAt(0).position.y)) {
+			spaces[i] = 2;
+			index = i;
+		}
+		if (trajan_Spaces[i].contains(currentTile.position.x, currentTile.position.y)) {
+			spaces[i] = 1;
+		}
+	}
+	//OK THIS LOGIC IS TO MOVE THE "TILE" TO THE ARCH POSITION AND MOVE THE ARCH TO THE
+	//NEXT SPOT!!
+	if (spaces[index] = 2) {
+		if (G.moveTile(currentTile, G.phaser.trajan.getAt(0), spaces)) {
+			G.moveArch(G.phaser.trajan.getAt(0), trajan_Spaces, spaces, index);
+		}
+	}
+}
+
+Game.prototype.moveTile = function(tile, arch, spaces) {
+	tile.position.x = arch.position.x;
+	tile.position.y = arch.position.y;
+	tile.angle = arch.angle;
+	spaces[0] = 1;
+	G.phaser.gameState = "bonusAction";
+	return true;
+}
+
+Game.prototype.moveArch = function(arch, trajanSpaces, spaces, index) {
+	arch.position.x = trajanSpaces[index += 1].x;
+	arch.position.y = trajanSpaces[index].y;
+	if (index == 0 || index == 3) {
+		arch.angle = -28;
+	} else if (index == 1 || index == 4) {
+		arch.angle = 28;
+	} else if (index == 2 || index == 5) {
+		arch.angle = 0;
+	}
+	spaces[index] = 2;
 }
